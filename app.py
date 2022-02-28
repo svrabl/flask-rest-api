222dc0f2cb7098ce7c7dc50e891ae5060f24ac5c
svrabl <8426120+svrabl@users.noreply.github.com>
2022-02-28 16:30:57 +0100

    Added PostgreSQL env.variable and psycopg2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 
 from flask import Flask
 from flask_restful import Api
@@ -12,6 +14,12 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
